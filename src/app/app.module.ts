ee73a9d3d958d1a1ace24470755da42da01f16fb
abhilashlegend <abhilash456a@live.com>
2023-07-05 12:33:22 +0530

    Added breadcumbs, about, contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
