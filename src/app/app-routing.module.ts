90b16acba250fc6ac1ad205ff905423b69ba7f40
abhilashlegend <abhilash456a@live.com>
2023-07-06 22:16:52 +0530

    Added login pages for admin and user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { LoginComponent as UserLoginComponent } from './pages/login/login.component';
 import { LoginComponent } from './pages/admin/login/login.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: "products", component: ProductsComponent, data: { title: "Products" } },
   { path: "about", component: AboutComponent, data: { title: "About" } },
   { path: "contact", component: ContactComponent, data: { title: "Contact" } },
+  { path: "login", component: UserLoginComponent, data: { title: "Login" } },
   { path: "admin", children: [
     { path: "login", component: LoginComponent, data: { title: "Admin Login" } }
   ]}
